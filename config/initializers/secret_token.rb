d5f4d8c42de2d1367aa4884bb4ab844c9cedea3a
Matt Broten <mattbroten@gmail.com>
2012-02-22 19:49:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiTwitterManager::Application.config.secret_token = '33943ddbcb2e9f410dd635c2f8a3f0f543aa565c8c166d37e44b7686f95bfd86bd44545bcc6cb3e708ab8800dab4bc6e4146b4e072e398399396956b28165846'
