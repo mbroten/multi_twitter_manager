d6edcff5ce6f6c2a26cd06b89542a8701cbe79a8
Matt Broten <mattbroten@gmail.com>
2012-02-22 23:17:17 -0800

    Set up demo of Devise (user sign up/sign in).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 MultiTwitterManager::Application.routes.draw do
-  root :to => "home#index"
 
+  root :to => "home#index"
   devise_for :users
+  resources :users, :only => :show
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
